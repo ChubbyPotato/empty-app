9f20f1f9c8a7b942fe3f6b1f86857b69af204223
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-09-11 14:40:46 -0400

    Updated from Brython Server: 9/11/2015 2:40:46 PM

--- turtorial1.py
+++ turtorial1.py
@@ -10,11 +10,15 @@ black = Color(0x000000, 1.0)
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(10, black)
 # A graphics asset that represents a rectangle
-rectangle = RectangleAsset(100, 200, thinline, green)
+rectangle = RectangleAsset(100, 200, thinline, red)
+circle= CircleAsset(90, thinline, green)
 
 
 # Now display a rectangle
-Sprite(rectangle)
+Sprite(rectangle, (200,60))
+Sprite(circle, (200,50))
+
+
 
 myapp = App()
 myapp.run()
